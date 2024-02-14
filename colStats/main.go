fe587f3c1d60c46bff3d52db62cb8429cf63fdd3
Eduardo Luz <luz.eduardo@gmail.com>
2024-02-14 23:32:16 +0000

    main body draft

---
+++ colStats/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	op := flag.String("op", "sum", "Operation to be executed")
+	column := flag.Int("col", 1, "CSV column on which to execute operation")
+
+	flag.Parse()
+
+	if err := run(flag.Args(), *op, *column, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filenames []string, op string, column int, out io.Writer) error {
+	return nil
+}
