408b9a7f05e6e72aa0348c1e7ffade3f5a4f90be
Eduardo Luz <luz.eduardo@gmail.com>
2024-02-15 10:19:12 +0000

    iterate over files and consolidate data

--- colStats/main.go
+++ colStats/main.go
@@ -20,5 +20,42 @@ func main() {
 }
 
 func run(filenames []string, op string, column int, out io.Writer) error {
-	return nil
+	var opFunc statsFunc
+	if len(filenames) == 0 {
+		return ErrNoFiles
+	}
+	if column < 1 {
+		return fmt.Errorf("%w: %d", ErrInvalidColumn, column)
+	}
+
+	switch op {
+	case "sum":
+		opFunc = sum
+	case "avg":
+		opFunc = avg
+	default:
+		return fmt.Errorf("%w: %s", ErrInvalidOperation, op)
+	}
+
+	consolidate := make([]float64, 0)
+	for _, fname := range filenames {
+		f, err := os.Open(fname)
+		if err != nil {
+			return fmt.Errorf("Cannot open the file: %w", err)
+		}
+
+		data, err := csv2float(f, column)
+		if err != nil {
+			return err
+		}
+
+		if err := f.Close(); err != nil {
+			return err
+		}
+
+		consolidate = append(consolidate, data...)
+	}
+
+	_, err := fmt.Fprintln(out, opFunc(consolidate))
+	return err
 }
