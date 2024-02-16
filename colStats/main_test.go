4f1ab865e29af1f1c3041efb8f1b94dd0336b277
Eduardo Luz <luz.eduardo@gmail.com>
2024-02-16 12:42:13 +0000

    adds integration test

---
+++ colStats/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	testCases := []struct {
+		name   string
+		col    int
+		op     string
+		exp    string
+		files  []string
+		expErr error
+	}{
+		{
+			name: "RunAvg1File", col: 3, op: "avg",
+			exp:    "227.6\n",
+			files:  []string{"./testdata/example.csv"},
+			expErr: nil,
+		},
+		{
+			name: "RunAvgMultiFiles", col: 3, op: "avg",
+			exp: "233.84\n",
+			files: []string{
+				"./testdata/example.csv",
+				"./testdata/example2.csv",
+			},
+			expErr: nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var res bytes.Buffer
+			err := run(tc.files, tc.op, tc.col, &res)
+			if tc.expErr != nil {
+				if err != nil {
+					t.Errorf("Expected error. Got nil instead")
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("Expected error %q, got %q instead.", tc.expErr, err)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("Unexpected error: %q", err)
+			}
+
+			if res.String() != tc.exp {
+				t.Errorf("Expected %q, got %q instead", tc.exp, &res)
+			}
+		})
+	}
+}
